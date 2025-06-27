b1eea776a0e59e942653b65474cc21760d53199c
Joao Inacio <joaoinacio322@gmail.com>
2025-06-27 11:15:43 -0300

    Adicionando função para comprar outros tipos de ingressos

--- js/app.js
+++ js/app.js
@@ -4,7 +4,10 @@ function comprar(){
 
     if(tiposIngressos.value == 'pista'){
         comprarPista(quantidade);
-
+    } else if (tiposIngressos.value == 'superior'){
+        comprarSuperior(quantidade);
+    } else if (tiposIngressos.value == 'inferior'){
+        comprarInferior(quantidade);
     }
 
 }
@@ -16,6 +19,29 @@ function comprarPista(quantidade){
     }else{
         qtdPista = qtdPista - quantidade;
         document.getElementById('qtd-pista').textContent = qtdPista;
+        alert('Compra realizada com sucesso!');
+
+    }
+}
+
+function comprarSuperior(quantidade){
+    let qtdSuperior = parseInt(document.getElementById('qtd-superior').textContent);
+    if(quantidade > qtdSuperior){
+        alert('Quantidade insuficiente de ingressos para a Cadeira superior');
+    }else{
+        qtdSuperior = qtdSuperior - quantidade;
+        document.getElementById('qtd-superior').textContent = qtdSuperior;
+        alert('Compra realizada com sucesso!');
+    }
+} 
+
+function comprarInferior(quantidade){
+    let qtdInferior = parseInt(document.getElementById('qtd-inferior').textContent);
+    if(quantidade > qtdInferior){
+        alert('Quantidade insuficiente de ingressos para a Cadeira Inferior');
+    }else{
+        qtdInferior = qtdInferior - quantidade;
+        document.getElementById('qtd-inferior').textContent = qtdInferior;
         alert('Compra realizada com sucesso!');
 
     }
